acebea8a4121b0b13141b7efb4da58c8fc254288
Enckka <mail.nicolas.belurier@gmail.com>
2022-10-31 17:22:00 +0100

    First commit

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,13 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from '../logo.svg';
+import '../App.css';
 
 function App() {
   return (
-    <div className="App">
+    <div className=".App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Hitler did nothing wrong.
         </p>
         <a
           className="App-link"
